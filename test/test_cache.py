8cd2f617ddd6e638b59fe0e7c2b81144375b23d8
Siddhant <gsiddhant159@gmail.com>
2021-06-03 22:54:10 +0530

    feat:cache test added

--- test/test_cache.py
+++ test/test_cache.py
@@ -1,7 +1,25 @@
-# from dictCLI import cache
-# import unittest 
+from dictCLI import cache,core
+import unittest 
+import json 
+import os 
+class TestCache(unittest.TestCase):
+    def setUp(self):
+        self.data_dir = cache.get_data_dir()
 
-# class TestCache(unittest.TestCase):
-#     def test_get_data_dir(self):
-#         result = cache.get_data_dir()
-#        self.assertEqual(result,r'C:\Users\Siddhant\AppData\Local\dictCLI')
+    def test_get_data_dir(self):
+        expected = os.path.join(os.environ.get('LOCALAPPDATA',''),'dictCLI')
+        self.assertEqual(self.data_dir,expected)
+
+
+    def test_word_cache(self):
+        test_json = {'testkey':'testvalue'}
+        cache.cache_meaning(test_json,'test_word')
+        filepath = os.path.join(self.data_dir,'word_cache','test_word.json')
+        self.assertTrue( os.path.isfile(filepath))
+
+        with open(filepath) as f:
+            self.assertDictEqual(test_json,json.load(f))
+
+
+if __name__ == "__main__":
+    unittest.main()
