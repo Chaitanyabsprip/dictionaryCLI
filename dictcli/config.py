ad88b10325e8b02426c9a931d547efd2aa562cc8
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2021-06-11 02:35:06 +0530

    ported to a config.yml

--- dictcli/config.py
+++ dictcli/config.py
@@ -1,10 +1,27 @@
-import yaml
+from yaml import FullLoader, load
 
-if __name__ == '__main__':
 
-    stream = open("../config.yml", 'r')
-    dictionary = yaml.load(stream)
-    # for key, value in dictionary.items():
-    #     print(key + " : " + str(value))
-    print(dictionary.items())
-    stream.close()
+class Config:
+    __instance = None
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super(Config, cls).__new__(cls)
+            cls.source = "wiktionary"
+            cls.commands = {
+                'help': ['h', 'help'],
+                'quit': ['q', 'exit', 'quit'],
+                'search': ['s', 'search'],
+                'flip': {
+                    'cmd': ['f', 'flip'],
+                    'randomize': ['r', 'random'],
+                    'next': ['n', 'j', ''],
+                    'prev': ['p', 'k', '.']
+                }
+            }
+            with open("config.yml", 'r') as f:
+                config = load(f, Loader=FullLoader)
+                cls.source = config['source']
+                cls.commands = config['commands']
+
+        # return cls.__instance
