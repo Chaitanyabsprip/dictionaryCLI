637e417bb39d94013c2a9d2c7527f72b4cd65ba7
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2021-06-14 04:45:40 +0530

    added check for internet before querying API

--- dictcli/util.py
+++ dictcli/util.py
@@ -1,7 +1,21 @@
+import socket
 from sys import exit
 from typing import List
 
 from dictcli.config import Config
+
+
+def is_connected():
+    try:
+        # connect to the host -- tells us if the host is actually
+        # reachable
+        sock = socket.create_connection(("www.google.com", 80))
+        if sock is not None:
+            sock.close
+        return True
+    except OSError:
+        pass
+    return False
 
 
 def get_mode(inp: str) -> str:
